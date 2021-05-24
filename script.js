1f22f398f9cba3e09972a31dbc78fb0e20d9bd5f
justcodingman <joginder9868@outlook.com>
2021-05-24 07:57:30 +0530

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContainer = document.getElementById("bookmarks-container");
 
+let bookmarks = [];
+
 // Show Modal , Focus on Input
 
 function showModal() {
@@ -40,6 +42,71 @@ function validate(nameValue, urlValue) {
   //
   return true;
 }
+// Build Bookmarks DOM
+function buildBookmarks() {
+  //Remove all bookmark elements
+  bookmarksContainer.textContent = ""; //
+  //Build items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    //Item
+    const item = document.createElement("div");
+    item.classList.add("item");
+    //Close Icon
+    const closeIcon = document.createElement("i");
+    closeIcon.classList.add("fas", "fa-times");
+    closeIcon.setAttribute("title", "Delete Bookmark");
+    closeIcon.setAttribute("onclick", `deleteBookmark('${url}')`);
+    //Favicon /Link Container
+    const linkInfo = document.createElement("div");
+    linkInfo.classList.add("name");
+    //Favicon
+    const favicon = document.createElement("img");
+    favicon.setAttribute(
+      "src",
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    ); // for local favicon.setAttribute("src", `favicon.png`)
+    favicon.setAttribute("alt", "Favicon");
+    //Link
+    const link = document.createElement("a");
+    link.setAttribute("href", `${url}`);
+    link.setAttribute("target", "_blank");
+    link.textContent = name;
+    //Append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+//Fetch Bookmarks
+function fetchBookmark(e) {
+  //Get bookmarks from localstorage if available
+  if (localStorage.getItem("bookmarks")) {
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    //Create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: "jogindra kumar",
+        url: "https://justcodingman.com",
+      },
+    ];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+  buildBookmarks();
+}
+
+//Delete Bookmark
+function deleteBookmark(url) {
+  bookmarks.forEach((bookmark, i) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(i, 1);
+    }
+  });
+  //update bookmarks array in localStorage, re-populate DOM
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmark();
+}
 
 // Handle Data from form
 
@@ -50,11 +117,24 @@ function storeBookmark(e) {
   if (!urlValue.includes("https://") && !urlValue.includes("http://")) {
     urlValue = `https://${urlValue}`;
   }
-  console.log(nameValue, urlValue);
+
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmark();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
 }
 
 //Event Listener
 bookmarkForm.addEventListener("submit", storeBookmark);
+
+// On load , Fetch Bookmarks
+fetchBookmark();
